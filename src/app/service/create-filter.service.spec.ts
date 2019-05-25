909211e3a6af403924149b35929a3b2eb31548e6
smuraga <smuraga.darya@gmail.com>
2019-05-25 20:30:03 +0300

    delete create-filter

--- src/app/service/create-filter.service.spec.ts
+++
@@ -1,7 +0,0 @@
-import { CreateFilter } from './create-filter.service';
-
-describe('CreateFilter', () => {
-  it('should create an instance', () => {
-    expect(new CreateFilter()).toBeTruthy();
-  });
-});
